4f679d74b397f3ef0544eaebd7ed29e2323138cf
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-12 17:04:03 -0400

    Load scores on page

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// function getHumanChoice() 
+//     let input = prompt("Choose your warrior!")
+//     let choice = input.toLowerCase();
+//     if (choice === "rock" || choice === "paper" || choice === "scissors") {
+//         return(choice)
+//     }
+//     else {
+//         return(null)
+//     }
+// }
+
 function getComputerChoice() {
     const random = Math.random()
     if (random < 0.33) {
@@ -11,19 +22,77 @@ function getComputerChoice() {
     }
 }
 
-// function getHumanChoice() {
-//     let input = prompt("Choose your warrior!")
-//     let choice = input.toLowerCase();
-//     if (choice === "rock" || choice === "paper" || choice === "scissors") {
-//         return(choice)
-//     }
-//     else {
-//         return(null)
-//     }
-// }
 
+// Declare variables for button calls 
 let humanScore = 0
 let computerScore = 0
+
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
+
+let warriors = document.querySelector("#warriors");
+let results = document.querySelector("#results");
+
+let scoreBoardHuman = document.createElement("h2")
+let scoreBoardComputer = document.createElement("h2")
+
+scoreBoardHuman.textContent = ("Your score: " + humanScore);
+scoreBoardComputer.textContent = ("Computer Score: " + computerScore);
+results.appendChild(scoreBoardHuman);
+results.appendChild(scoreBoardComputer);
+
+
+
+warriors.addEventListener("click", function(event) {
+    let target = event.target
+    switch(target.id) {
+        case "rock":
+            playRound("rock", getComputerChoice())
+            break;
+        case "paper":
+            playRound("paper", getComputerChoice())
+            break;
+        case "scissors":
+            playRound("scissors", getComputerChoice())
+            break;
+    }
+});
+
+
+function playRound(humanChoice, computerChoice) {
+    let plays = 1
+    
+    if ((computerChoice == "Scissors" && humanChoice == "rock") 
+        || (computerChoice == "Rock" && humanChoice == "paper")
+        || (computerChoice == "Paper" && humanChoice == "scissors")) {  
+            humanScore = humanScore + 1;
+            plays = (plays + 1);
+            alert("You win!");
+            return(plays);
+    }
+    else if ((computerChoice == "Scissors" && humanChoice == "paper") 
+        || (computerChoice == "Rock" && humanChoice == "scissors") 
+        || (computerChoice == "Paper" && humanChoice == "rock")) {
+            computerScore = computerScore + 1;
+            plays = (plays + 1);
+            alert("You lose!");
+            return(plays);
+    }
+    else {
+        alert("Tie!")
+        plays = (plays + 1);
+        return(plays);
+    }
+
+}
+    
+    
+
+
+
+
+
 
 // function playGame() {    
     
@@ -43,65 +112,3 @@ let computerScore = 0
     // else {
     //     alert ("IT WAS A TIE")
     // }
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == null) {
-            alert("Invalid entry")
-        }
-        else if ((computerChoice == "Scissors" && humanChoice == "rock") 
-            || (computerChoice == "Rock" && humanChoice == "paper")
-            || (computerChoice == "Paper" && humanChoice == "scissors")) {  
-                humanScore = humanScore + 1;
-                plays = (plays + 1);
-                alert("You win!");
-                console.log("Your score: " + humanScore);
-                console.log("Computer Score: " + computerScore);
-                return(plays);
-        }
-        else if ((computerChoice == "Scissors" && humanChoice == "paper") 
-            || (computerChoice == "Rock" && humanChoice == "scissors") 
-            || (computerChoice == "Paper" && humanChoice == "rock")) {
-                computerScore = computerScore + 1;
-                plays = (plays + 1);
-                alert("You lose!");
-                console.log("Your score: " + humanScore);
-                console.log("Computer Score: " + computerScore);
-                return(plays);
-        }
-        else {
-            alert("Tie!")
-            plays = (plays + 1);
-            console.log("Your score: " + humanScore);
-            console.log("Computer Score: " + computerScore);
-            return(plays);
-        }
-    }
-    
-    
-
-
-
-// Declare variables for button calls 
-let rock = document.querySelector("#rock");
-let paper = document.querySelector("#paper");
-let scissors = document.querySelector("#scissors");
-
-let warriors = document.querySelector("#warriors");
-//Declare variables for button onClicks
-warriors.addEventListener("click", function(event) {
-    let target = event.target
-    switch(target.id) {
-        case "rock":
-            console.log("BBBBB")
-            playRound("rock", getComputerChoice())
-            break;
-        case "paper":
-            playRound("paper", getComputerChoice())
-            break;
-        case "scissors":
-            playRound("scissors", getComputerChoice())
-            break;
-    }
-});
-
-//Create functions for button clicks 
